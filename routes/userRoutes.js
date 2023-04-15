612b5aac6ac690686b8a3377bae0df5f225f3ed3
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-04-16 00:33:55 +0530

    map/filter and loop nested array inside object -// Write a query to retrieve all documents where the score in subject 1 is greater than 90.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,8 +15,7 @@ router.get("/all", async (req, res) => {
   }
 });
 
-// Write a query to find all the users who live in state "State 5".
-//a
+// Write a query to retrieve all documents where the score in subject 1 is greater than 90.
 //
 router.get("/test", async (req, res) => {
   try {
@@ -25,12 +24,26 @@ router.get("/test", async (req, res) => {
     let filterUser = [];
     //
     allUsers.map(function (user) {
+      //
       if (user.address.state === "State 7") {
-        filterUser.push(user);
+        //"State 7"--if statement ta deoa-- just data kom dekhar jonno
+        for (let index = 0; index < user.grades.length; index++) {
+          //
+          if (
+            user.grades[index].subject === "Subject 1" &&
+            user.grades[index].score >= 90
+          ) {
+            // console.log(user.grades[index].score);
+            //
+            console.log(user.grades[index]);
+            //
+            filterUser.push(user);
+          }
+        }
       }
     });
     //
-    console.log(filterUser.length);
+    console.log(filterUser);
     //
     res.send(filterUser);
   } catch (error) {
