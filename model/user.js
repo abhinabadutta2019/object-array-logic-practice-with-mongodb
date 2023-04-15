71ae641b8823172e923d641ccafa3489d8b71248
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-04-15 14:56:48 +0530

    document genarator-portion--generateDocs() function--commented out- in model/user.js

--- model/user.js
+++ model/user.js
@@ -19,32 +19,32 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-function generateDocs(numDocs) {
-  const docs = [];
-  for (let i = 0; i < numDocs; i++) {
-    const doc = {
-      name: `Person ${i + 1}`,
-      age: Math.floor(Math.random() * 50) + 20,
-      address: {
-        street: `Street ${i + 1}`,
-        city: `City ${i + 1}`,
-        state: `State ${i + 1}`,
-        zip: `${Math.floor(Math.random() * 90000) + 10000}`,
-      },
-      grades: [],
-    };
-    for (let j = 0; j < 5; j++) {
-      doc.grades.push({
-        subject: `Subject ${j + 1}`,
-        score: Math.floor(Math.random() * 101),
-      });
-    }
-    docs.push(doc);
-  }
-  return docs;
-}
-//
-// const docs = generateDocs(1000);
+// function generateDocs(numDocs) {
+//   const docs = [];
+//   for (let i = 0; i < numDocs; i++) {
+//     const doc = {
+//       name: `Person ${i + 1}`,
+//       age: Math.floor(Math.random() * 50) + 20,
+//       address: {
+//         street: `Street ${i + 1}`,
+//         city: `City ${i + 1}`,
+//         state: `State ${i + 1}`,
+//         zip: `${Math.floor(Math.random() * 90000) + 10000}`,
+//       },
+//       grades: [],
+//     };
+//     for (let j = 0; j < 5; j++) {
+//       doc.grades.push({
+//         subject: `Subject ${j + 1}`,
+//         score: Math.floor(Math.random() * 101),
+//       });
+//     }
+//     docs.push(doc);
+//   }
+//   return docs;
+// }
+// //
+// const docs = generateDocs(200);
 // User.insertMany(docs)
 //   .then(() => console.log("Documents inserted successfully"))
 //   .catch((err) => console.error("Error inserting documents", err));
