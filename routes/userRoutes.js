87587f48fa9d56c7a1f97470b095b6ead1aaee72
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-04-15 23:14:27 +0530

    //Write a query to find all the users who are older than 60 years of age.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,56 +15,27 @@ router.get("/all", async (req, res) => {
   }
 });
 
-//findOne vs findOneLean--theory
-
-// When using the findOne method in Mongoose, it returns a Mongoose document, which is a special object that wraps the plain JavaScript object returned from MongoDB and adds extra functionality like change tracking and middleware.
-
-// However, these extra features come at a cost: Mongoose documents are heavier and slower than plain JavaScript objects, and they can cause memory problems when working with large datasets.
-
-// In these cases, using findOne().lean() can be a better option, as it returns a plain JavaScript object instead of a Mongoose document. This can result in better performance, especially when working with large datasets, as the plain JavaScript objects are lighter and faster.
-//
-router.get("/compareFindOneMethods", async (req, res) => {
-  try {
-    // Using findOne
-    const user = await User.findOne();
-    console.log(user instanceof mongoose.Document); // true
-
-    // Using findOne().lean()
-    const userLean = await User.findOne().lean();
-    console.log(userLean instanceof mongoose.Document); // false
-
-    res.send("Comparison of findOne()- and findOne().lean() completed");
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-//This code measures the time it takes to execute findOne() and findOne().lean() and logs the results to the console. You can compare the results to see if there is a significant difference in performance
+//Write a query to find all the users who are older than 60 years of age.
 
 //
-router.get("/performance-test", async (req, res) => {
+router.get("/test", async (req, res) => {
   try {
-    console.time("findOne");
-    const user = await User.findOne();
-    console.timeEnd("findOne");
-
-    console.time("findOneLean");
-    const userLean = await User.findOne().lean();
-    console.timeEnd("findOneLean");
-
-    res.send({
-      user,
-      userLean,
+    const allUsers = await User.find().lean();
+    //
+    let filterUser = [];
+    //
+    allUsers.map(function (user) {
+      if (user.age > 60) {
+        filterUser.push(user);
+      }
     });
+    //
+    console.log(filterUser.length);
+    //
+    res.send(filterUser);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send(error);
   }
 });
 
-//findOne: 44.672ms
-// findOneLean: 35.884ms
-
-//
-// In this example, we use console.time() and console.timeEnd() to measure the time it takes to execute findOne() and findOne().lean(). The results of the performance measurement will be displayed in the console.
-//
 module.exports = router;
