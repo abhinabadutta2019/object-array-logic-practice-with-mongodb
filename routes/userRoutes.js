1506e2ffbb86deb4c50a38468b90297d3120f19f
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-04-17 03:29:34 +0530

    easier promts than last one -- to understand -regex, elemMatch,expr

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,142 +15,183 @@ router.get("/all", async (req, res) => {
   }
 });
 
-//if age more than 67
 //
 
-router.get("/test", async (req, res) => {
+//Find all students who have a score greater than or equal to 50 in "Subject 4" and a score less than or equal to 20 in "Subject 5".
+router.get("/test1", async (req, res) => {
   try {
-    const filteredUsers = await User.find({
-      $expr: {
-        $gt: ["$age", 67],
+    const students = await User.find({
+      grades: {
+        $elemMatch: { subject: "Subject 4", score: { $gte: 50 } },
+        $elemMatch: { subject: "Subject 5", score: { $lte: 20 } },
       },
     });
-    res.send(filteredUsers);
-  } catch (error) {
-    res.status(500).send(error);
+    res.status(200).json(students);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
   }
 });
 
-//Here's an example of a query to retrieve all documents where the score in subject 1 is greater than 90, inside a route:
-router.get("/test1", async (req, res) => {
+//Find all users who have at least one grade with Subject 4 greater than or equal to 90.
+
+router.get("/test2", async (req, res) => {
   try {
-    const filteredUsers = await User.find({
+    const users = await User.find({
+      grades: {
+        $elemMatch: { subject: "Subject 4", score: { $gte: 90 } },
+      },
+    });
+    res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//Find all users who have a grade with a score greater than or equal to 90 in "Subject 3" and a score less than or equal to 10 in "Subject 5".
+router.get("/test3", async (req, res) => {
+  try {
+    const users = await User.find({
       grades: {
         $elemMatch: {
           subject: "Subject 3",
-          score: { $gt: 90 },
+          score: { $gte: 90 },
         },
       },
-    });
-    res.send(filteredUsers);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-//Write a query to retrieve all documents where age is grater than 63 but less than 67
-router.get("/test2", async (req, res) => {
-  try {
-    const filteredUsers = await User.find({
-      age: {
-        $gt: 63,
-        $lt: 67,
-      },
-    });
-    res.send(filteredUsers);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-//with $expr Write a query to retrieve all documents where age is grater than 63 but less than 67
-router.get("/test3", async (req, res) => {
-  try {
-    const filteredUsers = await User.find({
-      $expr: {
-        $and: [{ $gt: ["$age", 63] }, { $lt: ["$age", 67] }],
-      },
-    });
-    res.send(filteredUsers);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-//Write a query to retrieve all documents where the zip code starts with "7"
-router.get("/test4", async (req, res) => {
-  try {
-    const filteredUsers = await User.find({
-      $expr: {
-        $eq: [{ $substr: ["$address.zip", 0, 1] }, "7"],
-      },
-    });
-    res.send(filteredUsers);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-//Write a query to retrieve all documents where the zip code starts with "7"
-router.get("/test5", async (req, res) => {
-  try {
-    const filteredUsers = await User.find({
-      "address.zip": {
-        $regex: "^7",
-      },
-    });
-    res.send(filteredUsers);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-////Write a query to retrieve all documents where the zip code starts with 1 to 3
-router.get("/test6", async (req, res) => {
-  try {
-    const filteredDocuments = await User.find({
-      "address.zip": { $regex: /^[1-3]/ },
-    });
-    res.send(filteredDocuments);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-//Here's an example of a route that retrieves all documents where the score in subject 4 is from 85 to 95 using
-
-router.get("/test7", async (req, res) => {
-  try {
-    const result = await User.find({
       grades: {
         $elemMatch: {
-          subject: "Subject 4",
-          score: { $gte: 85, $lte: 95 },
+          subject: "Subject 5",
+          score: { $lte: 10 },
         },
       },
     });
-    res.send(result);
+    res.json(users);
   } catch (error) {
-    res.status(500).send(error);
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//age grater than 67
+router.get("/test4", async (req, res) => {
+  try {
+    const users = await User.find({ age: { $gt: 67 } });
+    res.send(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
+
+//Write a query to retrieve all documents where the average score in all subjects is greater than 75.
+router.get("/test5", async (req, res) => {
+  try {
+    const result = await User.find({
+      $expr: {
+        $gt: [
+          {
+            $avg: "$grades.score",
+          },
+          75,
+        ],
+      },
+    });
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
+
+//How to retrieve all documents where the state is "State 1"?
+router.get("/test7", async (req, res) => {
+  try {
+    const documents = await User.find({ "address.state": "State 1" });
+    res.send(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+//Write a query to find all the users who have a total score greater than 390 across all subjects.
+
+router.get("/test8", async (req, res) => {
+  try {
+    const documents = await User.find({
+      $expr: {
+        $gt: [{ $sum: "$grades.score" }, 390],
+      },
+    });
+    res.send(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+//find all where state is 3 and zip starts with 7
+
+router.get("/test9", async (req, res) => {
+  try {
+    const documents = await User.find({
+      "address.state": "State 3",
+      "address.zip": /^7/,
+    });
+    res.send(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+//find all where state is 3 and zip starts with from 5 to 7
+
+router.get("/test10", async (req, res) => {
+  try {
+    const documents = await User.find({
+      "address.state": "State 3",
+      "address.zip": { $regex: "^[5-7]" },
+    });
+    res.send(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+//find all where state is 3  or 5 and zip starts with from 5 to 7
+router.get("/test11", async (req, res) => {
+  try {
+    const documents = await User.find({
+      $and: [
+        {
+          $or: [{ "address.state": "State 3" }, { "address.state": "State 5" }],
+        },
+        { "address.zip": { $regex: "^[5-7]" } },
+      ],
+    });
+    res.send(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
   }
 });
 
 //
-router.get("/test8", async (req, res) => {
+router.get("/test12", async (req, res) => {
   try {
-    const subject4Score = await User.find({
-      $expr: {
-        $and: [
-          { $gte: ["$grades.score", 85] },
-          { $lte: ["$grades.score", 95] },
-        ],
-      },
-      "grades.subject": "Subject 4",
+    const documents = await User.find({
+      $or: [{ "address.state": "State 3" }, { "address.state": "State 5" }],
+      "address.zip": { $regex: "^(3|5)" },
     });
-    res.send(subject4Score);
-  } catch (err) {
-    res.status(500).send({ message: err.message });
+    res.send(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
   }
 });
 
+//
 module.exports = router;
