3f742ed999f333c6cde8380e8d7cce54772a1a6b
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-04-16 22:28:42 +0530

    used object/array method--where the score in all subjects is greater than or equal to 70--question2--used mongoose---//Write a query to retrieve all documents where the score in at least three subjects is less than 50.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,18 +15,37 @@ router.get("/all", async (req, res) => {
   }
 });
 
+//Write a query to retrieve all documents where the score in at least three subjects is less than 50.
+//
+router.get("/threeOrMoreScoresBelow50", async (req, res) => {
+  try {
+    const result = await User.find({
+      $expr: {
+        $gt: [
+          {
+            $size: {
+              $filter: {
+                input: "$grades",
+                as: "grade",
+                cond: { $lt: ["$$grade.score", 50] },
+              },
+            },
+          },
+          3,
+        ],
+      },
+    });
+    res.send(result);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 //
 router.get("/mongo", async (req, res) => {
   try {
     const result = await User.find({
-      grades: {
-        $elemMatch: {
-          subject: "Subject 2",
-          score: { $lte: 50 },
-        },
-      },
+      grades: { $not: { $elemMatch: { score: { $lt: 50 } } } },
     });
-
     res.send(result);
   } catch (error) {
     res.status(500).send(error);
@@ -34,21 +53,32 @@ router.get("/mongo", async (req, res) => {
 });
 
 //
-// Write a query to retrieve all documents where the score in subject 2 is less than or equal to 50.
+// Write a query to retrieve all documents where the score in all subjects is greater than or equal to 70.
 //
 router.get("/test", async (req, res) => {
   try {
     const allUsers = await User.find().lean();
     //
     filterArray = [];
+    // let counter = 0;
+
     //
-
     allUsers.map(function (user) {
-      if (user.grades[1].score <= 50) {
-        filterArray.push(user);
-      }
+      let counter = 0;
+      //
+      user.grades.forEach((item) => {
+        //
+        // let counter = 0;
+        // console.log(item);
+        if (item.score > 70) {
+          counter = counter + 1;
+          console.log(counter);
+          if (counter >= 4) {
+            filterArray.push(user);
+          }
+        }
+      });
     });
-
     res.send(filterArray);
   } catch (error) {
     res.status(500).send(error);
