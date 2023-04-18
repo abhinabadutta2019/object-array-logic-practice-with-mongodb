a6a2cffb619131b7f328fe2b041a50d553066665
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-04-18 15:40:19 +0530

    aggregation study- basic study-youtube-hindi video

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,106 +14,172 @@ router.get("/all", async (req, res) => {
     res.status(500).send(error);
   }
 });
-
-//find user if age is more than 67 and state is 3
+//age is 67
+//aggrigation -match-
 router.get("/test1", async (req, res) => {
   try {
-    const users = await User.find({
-      age: { $gt: 67 },
-      "address.state": "State 3",
-    });
-    res.send(users);
-  } catch (e) {
-    res.send(e);
+    const allUsers = await User.aggregate([{ $match: { age: 67 } }]);
+
+    res.send(allUsers);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
-//find user if age is more than 67 and state is 3 and score in subject 1 is more than 35
+
+//aggrigation group
 router.get("/test2", async (req, res) => {
   try {
-    const users = await User.find({
-      $and: [
-        { age: { $gt: 27 } },
-        { "address.state": "State 3" },
-        {
-          grades: { $elemMatch: { subject: "Subject 1", score: { $gt: 35 } } },
-        },
-      ],
-    });
-    res.send(users);
-  } catch (e) {
-    res.send(e);
+    const allUsers = await User.aggregate([{ $group: { _id: "$age" } }]);
+
+    res.send(allUsers);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
-//find user if state is 3, Subject 1 score grater than 55 and subject 2 score grater than 50
+//group -age --then expression -name
 router.get("/test3", async (req, res) => {
   try {
-    const users = await User.find({
-      $and: [
-        { "address.state": "State 3" },
-        {
-          grades: { $elemMatch: { subject: "Subject 1", score: { $gt: 55 } } },
-        },
-        {
-          grades: { $elemMatch: { subject: "Subject 2", score: { $gt: 50 } } },
-        },
-      ],
-    });
-    res.send(users);
-  } catch (e) {
-    res.send(e);
+    const allUsers = await User.aggregate([
+      { $group: { _id: "$age", names: { $push: "$name" } } },
+    ]);
+
+    res.send(allUsers);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
-//2. find user if state is 3, Subject 1 score less than 11 OR subject 2 score grater than 90
+//
+// {
+//   "_id": 53,
+//   "names": [
+//       "Person 16",
+//       "Person 23"
+//   ]
+// }
+
+//
+//group -age --then expression -full document/$$ROOT
 router.get("/test4", async (req, res) => {
   try {
-    const users = await User.find({
-      $and: [
-        { "address.state": "State 3" },
-        {
-          $or: [
-            {
-              grades: {
-                $elemMatch: { subject: "Subject 1", score: { $lt: 11 } },
-              },
-            },
-            {
-              grades: {
-                $elemMatch: { subject: "Subject 2", score: { $gt: 90 } },
-              },
-            },
-          ],
-        },
-      ],
-    });
-    res.send(users);
-  } catch (e) {
-    res.send(e);
+    const allUsers = await User.aggregate([
+      { $group: { _id: "$age", fullDocU: { $push: "$$ROOT" } } },
+    ]);
+
+    res.send(allUsers);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
-//To find a user if the state is 3 and the score in any subject is less than 10, we can use the following query:
+//
+//first match then grounp then sort
 router.get("/test5", async (req, res) => {
   try {
-    const users = await User.find({
-      $and: [{ "address.state": "State 3" }, { "grades.score": { $lt: 3 } }],
-    });
-    res.send(users);
-  } catch (e) {
-    res.send(e);
+    const allUsers = await User.aggregate([
+      //match by address.state
+      // { $match: { "address.state": "State 14" } },
+
+      //match by age from 19-24
+      { $match: { age: { $gt: 18, $lt: 25 } } },
+      //then group by address.state
+      { $group: { _id: "$address.state", tataiCount: { $sum: 1 } } },
+      //sort
+      {
+        $sort: {
+          tataiCount: -1,
+        },
+      },
+      //tried to find max of that tatai count
+      { $group: { _id: "$age", maxTataiCount: { $max: "$tataiCount" } } },
+    ]);
+
+    res.send(allUsers);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
-//To find a user if the state is 33( acctuly no state 33) or the score in any subject is grater than 99
+//unwind and then group example
 router.get("/test6", async (req, res) => {
   try {
-    const users = await User.find({
-      $or: [{ "address.state": "State 33" }, { "grades.score": { $gt: 99 } }],
-    });
-    res.send(users);
-  } catch (e) {
-    res.send(e);
+    const allUsers = await User.aggregate([
+      //first unwind
+      { $unwind: "$grades" },
+      //second group
+      // { $group: { _id: "$age", grades: { $push: "$grades" } } },
+      { $group: { _id: "$grades", count: { $sum: 1 } } },
+    ]);
+
+    res.send(allUsers);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+//find avarage age of all
+router.get("/test7", async (req, res) => {
+  try {
+    const allUsers = await User.aggregate([
+      { $group: { _id: null, avarageAge: { $avg: "$age" } } },
+    ]);
+
+    res.send(allUsers);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+//
+//total number of items in all grades array combined
+router.get("/test8", async (req, res) => {
+  try {
+    //using unwind and then group
+
+    // const allUsers = await User.aggregate([
+    //   { $unwind: "$grades" },
+    //   { $group: { _id: null, count: { $sum: 1 } } },
+    // ]);
+
+    //no need to unwind here
+
+    const allUsers = await User.aggregate([
+      { $group: { _id: null, count: { $sum: { $size: "$grades" } } } },
+    ]);
+
+    res.send(allUsers);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//filter
+//if age is grater less than 25, give the avarage of all score
+router.get("/test9", async (req, res) => {
+  try {
+    const allUsers = await User.aggregate([
+      { $match: { age: { $lt: 25 } } },
+      { $unwind: "$grades" },
+      { $group: { _id: null, totalScore: { $avg: "$grades.score" } } },
+    ]);
+
+    res.send(allUsers);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//
+router.get("/test10", async (req, res) => {
+  try {
+    const allUsers = await User.aggregate([
+      //first unwind
+      { $unwind: "$grades" },
+      { $group: { _id: null, allSubjects: { $addToSet: "$grades" } } },
+    ]);
+
+    res.send(allUsers);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 //
