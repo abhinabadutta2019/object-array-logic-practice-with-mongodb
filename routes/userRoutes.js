8994e0b8d73be06503f8b0b5cb1bc9f2f402e40c
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-04-21 22:18:35 +0530

    solve-by gpt-How many people have a score of 50 or higher in Subject 4 or Subject 5,,What is the average score of people living in each city across all subjects

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -25,134 +25,7 @@ router.get("/test", async (req, res) => {
   }
 });
 
-// c. Get the count of persons living in each city, and sort the results by the city name in ascending order:
 router.get("/test1", async (req, res) => {
-  try {
-    const allUsers = await User.aggregate([
-      {
-        $group: {
-          _id: "$address.city",
-          count: { $sum: 1 },
-        },
-      },
-    ]);
-
-    res.send(allUsers);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-//
-// a. Get the highest score for each subject, across all persons, and sort the results by the highest score in descending order:
-// {
-//   $unwind: "$grades",
-// },
-// {
-//   $group: {
-//     _id: "$grades.subject",
-//     getHighest: { $max: "$grades.score" },
-//   },
-// },
-
-//
-//To group by state and find the highest score for each subject:
-//  {
-//   $unwind: "$grades",
-// },
-// {
-//   $group: {
-//     _id: { state: "$address.state", subject: "$grades.subject" },
-//     getmax: { $max: "$grades.score" },
-//   },
-// },
-// {
-//   $group: {
-//     _id: "$_id.state",
-//     subjects: { $push: { subject: "$_id.subject", getmax: "$getmax" } },
-//   },
-// },
-//
-//  //To group by state and find the average age of people in each state:
-//     // { $unwind: "$address" },
-//     {
-//       $group: {
-//         _id: "$address.state",
-//         avarageAge: { $avg: "$age" },
-//       },
-//     },
-
-//c. Get the top 3 scores for each person:
-// {
-//   $project: {
-//     name: 1,
-//     _id: 1,
-//     grades: { $slice: ["$grades", 3] },
-//   },
-// },
-//// Get the average score for each subject across all persons:
-// { $unwind: "$grades" },
-// {
-//   $group: {
-//     _id: "$grades.subject",
-//     total: { $avg: "$grades.score" },
-//   },
-// },
-// // a. Get the total number of grades for each person:
-// {
-//   $unwind: "$grades",
-// },
-// { $count: "grades" },
-//
-//Get the highest score for each subject:
-// {
-//   $unwind: "$grades",
-// },
-// {
-//   $group: {
-//     _id: "$grades.subject",
-//     highestScore: {
-//       $max: "$grades.score",
-//     },
-//   },
-// },
-///// Filter documents by score greater than 90 and get the total count
-//this correct- wrong my try-below
-// { $unwind: "$grades" },
-// {
-//   $match: {
-//     "grades.score": { $gt: 90 },
-//   },
-// },
-// {
-//   $count: "total",
-// },
-
-//wrong answer by me--although giving different result-Filter documents by score greater than 90 and get the total count:
-//  {
-//   $match: {
-//     "grades.score": { $gt: 90 },
-//   },
-// },
-// {
-//   $group: { _id: "$age", total: { $sum: 1 } },
-// },
-
-//Group documents by city and get the average age:
-// {
-//   $group: {
-//     _id: "$address.state",
-//     avgAge: { $avg: "$age" },
-//   },
-// },
-// //tar por sort by avgAge
-// {
-//   $sort: {
-//     avgAge: 1,
-//   },
-// },
-//
-router.get("/test2", async (req, res) => {
   try {
     const allUsers = await User.aggregate([]);
 
@@ -163,3 +36,114 @@ router.get("/test2", async (req, res) => {
 });
 //
 module.exports = router;
+//////////////////////////////////
+//
+//solve-by gpt-How many people have a score of 50 or higher in "Subject 4" or "Subject 5"-
+// {
+//   $unwind: {
+//     path: "$grades",
+//   },
+// },
+// {
+//   $match: {
+//     $and: [
+//       { "grades.score": { $gte: 50 } },
+//       {
+//         $or: [
+//           { "grades.subject": "Subject 1" },
+//           { "grades.subject": "Subject 2" },
+//         ],
+//       },
+//     ],
+//   },
+// },
+// {
+//   $group: {
+//     _id: "$name",
+//     count: { $sum: 1 },
+//   },
+// },
+// {
+//   $group: {
+//     _id: null,
+//     uniqueCount: { $sum: 1 },
+//   },
+// },
+
+//////////////////////////////////////
+//
+//How many people have a score of 50 or higher in "Subject 4" or "Subject 5"
+//done by me-
+// {
+//   $unwind: {
+//     path: "$grades",
+//   },
+// },
+// {
+//   $match: {
+//     $and: [
+//       { "grades.score": { $gt: 49 } },
+//       {
+//         $or: [
+//           { "grades.subject": "Subject 1" },
+//           { "grades.subject": "Subject 2" },
+//         ],
+//       },
+//     ],
+//   },
+// },
+// {
+//   $group: {
+//     _id: "$name",
+//   },
+// },
+// {
+//   $count: "count",
+// },
+///////////////////////////////////
+//
+// What is the average score of people living in each city across all subjects?
+
+//my solve
+
+// {
+//   $unwind: {
+//     path: "$grades",
+//   },
+// },
+// {
+//   $group: {
+//     _id: "$address.city",
+//     getAvg: { $avg: "$grades.score" },
+//   },
+// },
+//////////////////////
+//
+// What is the average score of people living in each city across all subjects?
+
+//chat gpt solve
+
+// {
+//   $unwind: {
+//     path: "$grades",
+//   },
+// },
+// {
+//   $group: {
+//     _id: { city: "$address.city" },
+//     totalSum: { $sum: "$grades.score" },
+//     totalStudent: { $sum: 1 },
+//   },
+// },
+// {
+//   $project: {
+//     _id: 0,
+//     city: "$_id.city",
+//     avarageScore: {
+//       $divide: ["$totalSum", "$totalStudent"],
+//     },
+//   },
+// },
+//The _id field is set to 0 to exclude it from the output.
+//The city field is extracted from the _id field using the dot notation.
+//////////////////////////
