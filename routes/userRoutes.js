d4e3a8b595000a086e9efcd3a70ba71c311ee54c
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-04-22 22:47:31 +0530

    codedamn-- $match alltype examples

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,20 +15,15 @@ router.get("/all", async (req, res) => {
 });
 
 router.get("/test", async (req, res) => {
+  //gets all user age $lte  25
   try {
     const allUsers = await User.aggregate([
+      // {
+      //   $unwind: "$grades",
+      // },
+
       {
-        $unwind: "$grades",
-      },
-      {
-        $group: {
-          //eta constant bole -- 1ta object e result asche
-          _id: "randomID",
-          sumScores: { $sum: "$grades.score" },
-          maxScore: { $max: "$grades.score" },
-          minScores: { $min: "$grades.score" },
-          avgScore: { $avg: "$grades.score" },
-        },
+        $match: { age: { $lte: 25 } },
       },
     ]);
 
@@ -42,44 +37,17 @@ router.get("/test", async (req, res) => {
 router.get("/test1", async (req, res) => {
   try {
     const allUsers = await User.aggregate([
+      //unwind diye array related example
+      //jotogulo grade score lessthan equal to 20--- same user er repeat hochhe- jeta group kore -- unique user bar kora jabe
       {
         $unwind: "$grades",
       },
+
       {
-        $group: {
-          //eta variable bole prottekta $name variable wise result asche- $name 200 ta bole total 200 ta result asche
-          _id: "$name",
-          sumScores: { $sum: "$grades.score" },
-          maxScore: { $max: "$grades.score" },
-          minScores: { $min: "$grades.score" },
-          avgScore: { $avg: "$grades.score" },
-        },
+        $match: { "grades.score": { $lte: 20 } },
       },
     ]);
 
-    res.send(allUsers);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-router.get("/test1", async (req, res) => {
-  try {
-    const allUsers = await User.aggregate([
-      {
-        $unwind: "$grades",
-      },
-      {
-        $group: {
-          //eta age wise segment kore total 48 ta result asche
-          _id: "$age",
-          sumScores: { $sum: "$grades.score" },
-          maxScore: { $max: "$grades.score" },
-          minScores: { $min: "$grades.score" },
-          avgScore: { $avg: "$grades.score" },
-        },
-      },
-    ]);
     res.send(allUsers);
   } catch (error) {
     res.status(500).send(error);
@@ -90,20 +58,14 @@ router.get("/test1", async (req, res) => {
 router.get("/test2", async (req, res) => {
   try {
     const allUsers = await User.aggregate([
+      //unwind diye array related example
+
       {
-        $group: {
-          //etai array nei bole $unwind er dorkar nei
-          //eta $address.state wise segment kore total 49 ta result asche
-          _id: "$address.city",
-          // sumScores: { $sum: "$grades.score" },
-          // maxScore: { $max: "$grades.score" },
-          // minScores: { $min: "$grades.score" },
-          // avgScore: { $avg: "$grades.score" },
-          //
-          count: { $sum: 1 },
-          //count 1 kora mane --- total kota oita royeche -- ekhane- address.state
-          //eta $address.state wise segment kore total 49 ta result asche
-        },
+        $unwind: "$grades",
+      },
+      //sob user er "Subject 1" er value asche
+      {
+        $match: { "grades.subject": "Subject 1" },
       },
     ]);
 
@@ -113,36 +75,41 @@ router.get("/test2", async (req, res) => {
   }
 });
 
+//match e duto condition
 router.get("/test3", async (req, res) => {
   try {
     const allUsers = await User.aggregate([
+      //unwind diye array related example
+
+      {
+        $unwind: "$grades",
+      },
+
+      {
+        $match: {
+          "grades.subject": "Subject 1",
+          //sob user , jader score  "Subject 1" e 20 er kom
+          "grades.score": { $lte: 20 },
+        },
+      },
+    ]);
+    res.send(allUsers);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get("/test4", async (req, res) => {
+  try {
+    const allUsers = await User.aggregate([
+      //object related / array related noi tai -- unwind lagche naa
       // {
       //   $unwind: "$grades",
       // },
+      //match kora hocche zip $gte: "50000", $lte: "70000"--
       {
-        $group: {
-          //eta $address.state wise segment kore total 20 ta result asche
-          _id: "$address.state",
-
-          //ei part ta etai dorkar nei
-          // sumScores: { $sum: "$grades.score" },
-          // maxScore: { $max: "$grades.score" },
-          // minScores: { $min: "$grades.score" },
-          // avgScore: { $avg: "$grades.score" },
-          //
-          counter: { $sum: 1 },
-        },
+        $match: { "address.zip": { $gte: "50000", $lte: "70000" } },
       },
-
-      //eta korle total kota $address.state ache paoa jabe - 200 ta paoar kotha
-      //
-      // {
-      //   $group: {
-      //     _id: "randomID",
-      //     count: { $sum: "$counter" },
-      //   },
-      //
-      // },
     ]);
     res.send(allUsers);
   } catch (error) {
