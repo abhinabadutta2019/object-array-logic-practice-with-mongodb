73165ec1d3f4e4e30315a4cbda5aab960c6bef35
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-04-27 12:57:40 +0530

    How many people have a score of 30 to 35 higher in Subject 1 and Subject 4?//eta- 2 bar aggrigate use korte hoyeche-- Which person has the lowest score in Subject 2 and the highest score in Subject 4?

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,82 +14,132 @@ router.get("/all", async (req, res) => {
   }
 });
 
-router.get("/test2", async (req, res) => {
-  try {
-    const allUsers = await User.aggregate([
-      //unwind
-      {
-        $unwind: "$grades",
-      },
-      //match --zip that starts with -5
-      {
-        $match: { "address.zip": /^5/ },
-      },
-
-      //eta korle prottekta individual _id er -- thoseSubjects-array te dhukche
-      {
-        $group: { _id: "random", thoseSubjects: { $push: "$grades.score" } },
-      },
-      //
-      {
-        $project: {
-          result: {
-            $sortArray: { input: "$thoseSubjects", sortBy: -1 },
-          },
-        },
-      },
-    ]);
-
-    res.send(allUsers);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-//example 1
+//How many people have a score of 20 or lower in all subjects combined
 router.get("/test", async (req, res) => {
   try {
-    const allUsers = await User.aggregate([
-      {
-        $addFields: {
-          totalScore: { $sum: "$grades.score" },
-        },
-      },
-      //
-      {
-        $addFields: {
-          newField: "Hello World",
-        },
-      },
-    ]);
+    const allUsers = await User.aggregate([]);
 
     res.send(allUsers);
   } catch (error) {
     res.status(500).send(error);
   }
 });
-//emaxple 2
+// How many people have a score of 30 to 35 higher in "Subject 1" and "Subject 4"?
 router.get("/test1", async (req, res) => {
   try {
     const allUsers = await User.aggregate([
       {
+        $unwind: {
+          path: "$grades",
+        },
+      },
+      {
         $match: {
-          age: {
-            $gt: 24,
-            $lt: 30,
+          $or: [
+            {
+              "grades.subject": "Subject 1",
+            },
+            {
+              "grades.subject": "Subject 2",
+            },
+          ],
+        },
+      },
+      {
+        $match: {
+          "grades.score": {
+            $gt: 30,
+            $lt: 75,
           },
         },
       },
       {
         $group: {
-          _id: "$age",
-          counter: {
+          _id: "$name",
+          fieldN: {
+            $sum: 1,
+          },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          fieldN: {
             $sum: 1,
           },
         },
       },
     ]);
 
+    res.send(allUsers);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+// Which person has the lowest score in "Subject 2" and the highest score in "Subject 4"?
+router.get("/test2", async (req, res) => {
+  try {
+    //
+    const highestSubject2 = await User.aggregate([
+      {
+        $unwind: {
+          path: "$grades",
+        },
+      },
+      {
+        $match: {
+          "grades.subject": "Subject 2",
+        },
+      },
+      {
+        $group: {
+          _id: "$_id",
+          highestScore: {
+            $max: "$grades.score",
+          },
+        },
+      },
+      {
+        $sort: {
+          highestScore: -1,
+        },
+      },
+      {
+        $limit: 1,
+      },
+    ]);
+    //
+    console.log(highestSubject2);
+    //
+    const lowestSubject3 = await User.aggregate([
+      {
+        $unwind: {
+          path: "$grades",
+        },
+      },
+      {
+        $match: {
+          "grades.subject": "Subject 2",
+        },
+      },
+      {
+        $group: {
+          _id: "$_id",
+          highestScore: {
+            $min: "$grades.score",
+          },
+        },
+      },
+      {
+        $sort: {
+          highestScore: 1,
+        },
+      },
+      {
+        $limit: 1,
+      },
+    ]);
+    console.log(lowestSubject3);
     res.send(allUsers);
   } catch (error) {
     res.status(500).send(error);
