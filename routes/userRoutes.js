3dea1f0694ba267edce12fc2eccb5e84984a5681
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-04-29 20:37:56 +0530

    solved using filter method -- //How many people have a score of 30 or lower in Subject 2 and Subject 4?

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("../model/user");
-const e = require("express");
 const router = express.Router();
 
 router.get("/all", async (req, res) => {
@@ -15,117 +14,54 @@ router.get("/all", async (req, res) => {
   }
 });
 
-//What is the average score of people aged 25 or younger in "Subject 3"?
-//for loop
-router.get("/find1", async (req, res) => {
+//How many people have a score of 30 or higher in "Subject 1" and "Subject 4"?
+
+router.get("/try1", async (req, res) => {
   try {
     const allUsers = await User.find({});
-    //
-    let ageLessThan = [];
-    //
-    for (let index = 0; index < allUsers.length; index++) {
-      //eta correct
-      const element = allUsers[index].grades;
 
-      // const element = allUsers[index];
+    let emptyArray = [];
 
-      //checking age
+    allUsers.filter(function (element) {
+      // console.log(element.grades);
+      //
+      let gradesArray = element.grades;
+      //
+      gradesArray.filter(function (eachGrade) {
+        // console.log(eachGrade);
 
-      if (allUsers[index].age < 26) {
         //
-        // console.log(allUsers[index]);
-        // console.log(element, "grades array");
 
-        //grades array portion
-        for (let j = 0; j < element.length; j++) {
-          const eachGradesObject = element[j];
+        //check if less than in subject2
+        if (eachGrade.subject === "Subject 2" && eachGrade.score < 30) {
+          // console.log(eachGrade);
+          // console.log(element.grades);
+          let fourthSubject = element.grades;
           //
-          // console.log(eachGradesObject, "eachGradesObject");
-          if (eachGradesObject.subject === "Subject 3") {
-            //
-            // console.log(eachGradesObject.score, "eachGradesObject");
-            //
-            ageLessThan.push(eachGradesObject.score);
-          }
+          fourthSubject.filter(function (eachMarks) {
+            // console.log(eachMarks);
+            if (eachMarks.subject === "Subject 4" && eachMarks.score < 30) {
+              // console.log(eachMarks);
+              emptyArray.push(element);
+            }
+          });
         }
-      }
-    }
-    //
-    // console.log(ageLessThan);
-    let sumageLessThan = ageLessThan.reduce(function (
-      accumulator,
-      currentValue
-    ) {
-      accumulator = accumulator + currentValue;
-      return accumulator;
+      });
+      // console.log(emptyArray);
     });
-    // console.log(sumageLessThan);
 
-    // let ageLessThanLength = ageLessThan.length;
-    // console.log(ageLessThanLength);
-
-    //
-    let ageLessThanAvarage = sumageLessThan / ageLessThan.length;
-    console.log(ageLessThanAvarage);
-    //
-
-    res.send();
+    res.send(emptyArray);
   } catch (error) {
     res.status(500).send(error);
   }
 });
 
-//What is the average score of people aged 25 or younger in "Subject 3"?
-//forEach
-router.get("/find2", async (req, res) => {
+router.get("/try2", async (req, res) => {
   try {
     const allUsers = await User.find({});
-    //foreach age
-    //array to store age<25
-    let ageLessThan = [];
-    allUsers.forEach(function (element) {
-      //
-      if (element.age < 25) {
-        // console.log(element);
-        //
-        ageLessThan.push(element);
-      }
-      //
-    });
-    //
 
-    //array to store subject 3 marks
-    let subject3Marks = [];
-
-    //
-    ageLessThan.forEach(function (params) {
-      //
-      // console.log(params.grades, "gradesArray Part");
-      //
-      let gradesArray = params.grades;
-      //
-      gradesArray.forEach(function (eachSubject) {
-        // console.log(eachSubject);
-        if (eachSubject.subject === "Subject 3") {
-          // console.log(eachSubject.score);
-          subject3Marks.push(eachSubject.score);
-        }
-      });
-    });
-    // console.log(subject3Marks);
-    //
-    //
-    let totalSum = 0;
-    //
-    subject3Marks.forEach(function (eachItem) {
-      totalSum = totalSum + eachItem;
-    });
-    console.log(totalSum);
-    //
-    let avarageScore = totalSum / subject3Marks.length;
-    console.log(avarageScore, "avarageScore");
-    //
-    res.send();
+    console.log(typeof allUsers);
+    res.send(allUsers);
   } catch (error) {
     res.status(500).send(error);
   }
