94c29d08348b97a792926f7b1bc76ed48562b593
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-05-11 22:14:12 +0530

    for,foreach, map, filter practice

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,139 +12,6 @@ router.get("/all", async (req, res) => {
     res.status(500).send(error);
   }
 });
-
-//////////////////////////////////////////////////////////////////////////////////////
-//Object groupping related-- practice
-///////////////////////////////////////////////////////////////////////////////////////
-
-//Group the objects by their age.
-//for loop
-router.get("/try1", async (req, res) => {
-  try {
-    const allUsers = await User.find({});
-    let outputObject = {};
-    //
-    for (let i = 0; i < allUsers.length; i++) {
-      const element = allUsers[i];
-      //
-      if (!outputObject[element.age]) {
-        outputObject[element.age] = [element];
-      } else {
-        outputObject[element.age].push(element);
-      }
-    }
-    res.send(outputObject);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-//Group the objects by their highest score in the grades array.
-//for loop
-router.get("/try2", async (req, res) => {
-  try {
-    const allUsers = await User.find({});
-    let outputObject = {};
-    //
-    for (let i = 0; i < allUsers.length; i++) {
-      const gradesArray = allUsers[i].grades;
-      //
-      let highScore = -1;
-      for (let j = 0; j < gradesArray.length; j++) {
-        let grade = gradesArray[j];
-        //
-        if (highScore < grade.score) {
-          highScore = grade.score;
-        }
-      }
-      //
-      // console.log(highScore);
-      if (!outputObject[highScore]) {
-        outputObject[highScore] = [allUsers[i].name];
-      }
-      //
-      else {
-        outputObject[highScore].push(allUsers[i].name);
-      }
-
-      // break;
-    }
-
-    res.send(outputObject);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-//group by age- subject 1 highest scorer
-//for loop
-router.get("/try3", async (req, res) => {
-  try {
-    const allUsers = await User.find({});
-    let outputObject = {};
-
-    for (let i = 0; i < allUsers.length; i++) {
-      const age = allUsers[i].age;
-      const gradesArray = allUsers[i].grades;
-      //
-      for (let j = 0; j < gradesArray.length; j++) {
-        const grade = gradesArray[j];
-
-        //
-        if (grade.subject === "Subject 1") {
-          // console.log(grade.subject);
-          if (!outputObject[age]) {
-            outputObject[age] = grade.score;
-          } else if (outputObject[age] && grade.score > outputObject[age]) {
-            outputObject[age] = grade.score;
-            //this not necessery
-            outputObject[`${age} scorer name`] = allUsers[i].name;
-            // console.log("Hi");
-          }
-        }
-      }
-      // break;
-    }
-
-    res.send(outputObject);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-//group by age- subject 1 highest scorer
-//foreach
-router.get("/try4", async (req, res) => {
-  try {
-    const allUsers = await User.find({});
-    let outputObject = {};
-    //
-    allUsers.forEach(function (oneObj) {
-      //
-      let gradesArray = oneObj.grades;
-      const age = oneObj.age;
-      //
-      gradesArray.forEach(function (grade) {
-        if (grade.subject === "Subject 1") {
-          if (!outputObject[age]) {
-            outputObject[age] = grade.score;
-          }
-          //
-          else if (outputObject[age] && grade.score > outputObject[age]) {
-            outputObject[age] = grade.score;
-            // console.log("HI");
-          }
-          // console.log(grade.score);
-        }
-      });
-    });
-
-    res.send(outputObject);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-//////////////////////////////////////////////////////////////////////////////////////
-//ends ---- Object groupping related-- practice
-///////////////////////////////////////////////////////////////////////////////////////
-
 //
 //////////////////////////////////////////////////////////////////////////////////////
 //starts-Array related-- map, filter -- practice
@@ -182,48 +49,148 @@ router.get("/try5", async (req, res) => {
     res.status(500).send(error);
   }
 });
-//
+//Find all the persons who have not scored less than 60 in any subject
+//foreach
 router.get("/try6", async (req, res) => {
   try {
     const allUsers = await User.find({});
 
-    res.send();
+    let outputArray = [];
+    //
+    allUsers.forEach(function (oneObj) {
+      let gradesArray = oneObj.grades;
+      //
+      let below = 0;
+      gradesArray.forEach(function (grades) {
+        //
+
+        if (grades.score < 45) {
+          below = below + 1;
+        }
+      });
+      if (below === 0) {
+        outputArray.push(oneObj);
+      }
+    });
+
+    res.send(outputArray);
   } catch (error) {
     res.status(500).send(error);
   }
 });
-//
+//Find all the persons who have not scored less than 60 in any subject
+//filter
 router.get("/try7", async (req, res) => {
   try {
     const allUsers = await User.find({});
-
-    res.send();
+    let outputArray = allUsers.filter(function (oneObj) {
+      let gradesArray = oneObj.grades;
+      //
+      let filterGrades = gradesArray.filter(function (grade) {
+        if (grade.score < 60) {
+          return true;
+        }
+      });
+      if (filterGrades.length === 0) {
+        return true;
+      }
+      // console.log();
+      // return;
+    });
+    res.send(outputArray);
   } catch (error) {
     res.status(500).send(error);
   }
 });
-//
+//find all the people who got a score of 90 or higher in two or more subject.
+
 router.get("/try8", async (req, res) => {
   try {
     const allUsers = await User.find({});
-
-    res.send();
+    //
+    let outerFilter = allUsers.filter(function (oneObj) {
+      //
+      let gradesArray = oneObj.grades;
+      //
+      let innerFilter = gradesArray.filter(function (grade) {
+        //
+        if (grade.score > 90) {
+          return true;
+        }
+      });
+      // console.log(innerFilter.length);
+      if (innerFilter.length >= 2) {
+        return true;
+      }
+    });
+    res.send(outerFilter);
   } catch (error) {
     res.status(500).send(error);
   }
 });
-//
+//Write a function that returns an array of objects with the name and highest scoring subject of each person.
+//map
 router.get("/try9", async (req, res) => {
   try {
     const allUsers = await User.find({});
-
-    res.send();
+    //
+    let outerMap = allUsers.map(function (oneObj) {
+      let gradesArray = oneObj.grades;
+      //
+      let highScore = -1;
+      gradesArray.forEach(function (grade) {
+        if (grade.score > highScore) {
+          highScore = grade.score;
+        }
+      });
+      // console.log(highScore);
+      return { name: oneObj.name, highScore };
+    });
+    //
+    res.send(outerMap);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+//create an array of objects containing the name and total score of each person.
+//also sorted
+router.get("/try10", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+    //
+    let outerMap = allUsers.map(function (oneObj) {
+      let gradesArray = oneObj.grades;
+      //
+      let totalScore = 0;
+      gradesArray.forEach(function (grade) {
+        //
+        totalScore = totalScore + grade.score;
+        //
+      });
+      //
+      if (totalScore) {
+        return { name: oneObj.name, sumOfScores: totalScore };
+      }
+      //
+    });
+    //
+    let sortedArray = outerMap.sort(function (a, b) {
+      return b.sumOfScores - a.sumOfScores;
+    });
+    res.send(sortedArray);
   } catch (error) {
     res.status(500).send(error);
   }
 });
 //
-
+router.get("/try11", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+    res.send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 /////////////////////////////////////////////////////////////////////////////////////////////
 //aggregate
 //////////////////////////////
